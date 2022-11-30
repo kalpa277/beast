bf29d70f2641810ce6137479440351cd633fc10b
kalpa277 <94000670+kalpa277@users.noreply.github.com>
2022-11-30 17:53:30 +0530

    Update views.py
    added print statement

--- appn/views.py
+++ appn/views.py
@@ -11,6 +11,7 @@ from django.http import Http404
 from rest_framework import mixins,generics
 class CourseListView(ModelViewSet):
     queryset=Course.objects.all()
+    print(queryset)
     serializer_class=CourseSerializer
     
     
