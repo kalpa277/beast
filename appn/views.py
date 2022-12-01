afef5d561eecf5e59862a5c7f8aa89a5b2d32d37
kalpa277 <kalpataru7905@gmail.com>
2022-12-01 15:59:37 +0530

    updated_views

--- appn/views.py
+++ appn/views.py
@@ -11,7 +11,7 @@ from django.http import Http404
 from rest_framework import mixins,generics
 class CourseListView(ModelViewSet):
     queryset=Course.objects.all()
-    print("queryset","queryset")
+    print("queryset","hii")
     serializer_class=CourseSerializer
     
     
